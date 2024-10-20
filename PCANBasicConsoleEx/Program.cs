6678447a010d64c97408e0bf98cbdfbd429a7804
92berra <ohyunwha@gmail.com>
2024-10-20 19:57:16 +0900

    Completed Feature: Initialization, Find 2nd Device

--- PCANBasicConsoleEx/Program.cs
+++ PCANBasicConsoleEx/Program.cs
@@ -8,41 +8,67 @@ using Peak.Can.Basic;
 namespace PCANBasicConsoleEx
 {
     using TPCANHandle = System.UInt16;
-    using TPCANBitrateFD = System.String;
-    using TPCANTimestampFD = System.UInt64;
 
     class Program
     {
-
         static void Main(string[] args)
         {
             TPCANHandle channel = PCANBasic.PCAN_USBBUS1;
-            TPCANBaudrate m_baurdrate = TPCANBaudrate.PCAN_BAUD_500K;
+            StringBuilder hardwareName = new StringBuilder(PCANBasic.MAX_LENGTH_HARDWARE_NAME);
 
-            TPCANStatus stsResult = PCANBasic.Initialize(channel, m_baurdrate);
+            uint controllerNumber;
+            TPCANHandle debugBus = PCANBasic.PCAN_NONEBUS;
+            ushort canChannel2 = PCANBasic.PCAN_NONEBUS;
 
-            Console.WriteLine($"Success to initialize {channel}.");
-
-        }
-
-        private TPCANStatus WriteFrame()
-        {
-            TPCANMsg CANMsg;
-
-            CANMsg = new TPCANMsg();
-            CANMsg.DATA = new byte[8];
-
-            CANMsg.ID = Convert.ToUInt32("5V Power", 16);
-            CANMsg.LEN = Convert.ToByte(8);
-            CANMsg.MSGTYPE = TPCANMessageType.PCAN_MESSAGE_EXTENDED;
-
-            for (int i = 0; i < 8; i++)
+            if (PCANBasic.GetValue(channel, 
+                                    TPCANParameter.PCAN_HARDWARE_NAME, 
+                                    hardwareName,
+                                    PCANBasic.MAX_LENGTH_HARDWARE_NAME) 
+                                    == TPCANStatus.PCAN_ERROR_OK)
             {
-                // random 
-                CANMsg.DATA[i] = Convert.ToByte();
+                if (hardwareName.ToString().Equals("PCAN-USB"))
+                {
+                    debugBus = channel;
+                }
             }
 
-            return PCANBasic.Write(channel, ref CANMsg);
+            if (PCANBasic.GetValue(channel, 
+                                    TPCANParameter.PCAN_CONTROLLER_NUMBER, 
+                                    out controllerNumber, 
+                                    sizeof(uint)) == TPCANStatus.PCAN_ERROR_OK)
+            {
+                if (controllerNumber == 1)
+                {
+                    canChannel2 = channel;
+                }
+            }
+
+
+            if (debugBus != PCANBasic.PCAN_NONEBUS)
+            {
+                Console.WriteLine("Single PCAN-USB for debugging (0x{0:X}) found. Starting to work . . .", debugBus);
+            }
+            else
+                Console.WriteLine("Error! Single PCAN-USB Channel was not found. Terminating . . .");
+
+
+            if (PCANBasic.GetValue(channel, 
+                                    TPCANParameter.PCAN_CONTROLLER_NUMBER, 
+                                    out controllerNumber, 
+                                    sizeof(uint)) == TPCANStatus.PCAN_ERROR_OK)
+            {
+                if (controllerNumber == 1)
+                {
+                    debugBus = channel;
+                }
+            }
+
+            if (canChannel2 != PCANBasic.PCAN_NONEBUS)
+            {
+                Console.WriteLine("CAN Controller found (handle 0x{0:X}). Starting to work . . .", canChannel2);
+            }
+            else
+                Console.WriteLine("Error! CAN Controller was not found. Terminating . . .");
         }
     }
 }
